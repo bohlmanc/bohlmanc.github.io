995c75dcf400ce2005524ba50ec5ba98c5255645
unknown <cody.bohlman@CODYB01.dev.wonderware.com>
2015-06-24 13:45:31 -0700

    Created 'scripts.js' to encapsulate the javascript code

---
+++ scripts.js
@@ -0,0 +1,57 @@
+function myFunction() {  
+    document.getElementById("output").innerHTML = ""
+    var willPlay = confirm("Would you like to play?")
+
+    if( willPlay) {
+        var age = prompt("Ok, what's your age?")
+        var string = "";
+        document.getElementById("output").innerHTML += "You are " + age + "\n"
+
+        var userAnswer = prompt("Do you want to race Bieber on stage?")
+        if(prompt === "yes"){
+            document.getElementById("output").innerHTML += "You raced Bieber!" + "\n"
+        }
+        else {
+        }
+
+        var feedback = prompt("How would you rate this game? (Out of 10)")
+        if(feedback > 8) {
+            document.getElementById("output").innerHTML += "\n\nThanks for playing!"
+        }
+        else {
+            document.getElementById("output").innerHTML += "\n\nOk, I'll keep working on it!"
+        }
+
+    }
+}
+        
+function clearText() {
+    document.getElementById("output").innerHTML = "";
+}
+
+function myOtherFunction() {
+    var square = document.getElementById("myCanvas");
+    square.style.background = "#a58744";
+    square.style.visibility = "visible";
+}
+function removeSquare() {
+    var square = document.getElementById("myCanvas");
+    square.style.background = "#fffffe";
+    square.style.visibility = "invisible";
+}
+
+function playGame() {
+    var gameBoard = document.getElementById("game");
+    if(gameBoard.getAttribute("backgroundColor") === "#fffffe") {
+        //confirm("Changing color ");
+        gameBoard.setAttribute("backgroundColor","#aaaaaa");   
+        gameBoard.style.backgroundColor = gameBoard.getAttribute("backgroundColor");
+
+    }
+    else {
+        //confirm("Other color");
+        gameBoard.setAttribute("backgroundColor","#fffffe"); 
+        gameBoard.style.backgroundColor = gameBoard.getAttribute("backgroundColor");
+
+    }
+}
