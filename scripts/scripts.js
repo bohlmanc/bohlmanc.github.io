fc8ad151dae7f2dc68e2af3c94788c6ab47d52fa
unknown <cody.bohlman@CODYB01.dev.wonderware.com>
2015-06-26 11:56:23 -0700

    Added table, adding capability

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -59,3 +59,84 @@ function playGame() {
 function hello() {
     confirm("You pressed the table!");   
 }
+
+var codyHours = [1,2,3,4,5,6,7,8,9,10];
+var momHours = [10,9,8,7,6,5,4,3,2,1];
+var dadHours = [5,5,5,5,5,5,5,5,5,5];
+
+function addCell() {
+    var user = document.getElementById("curUser");
+    var newChild = document.createElement("td");
+    var newCell = document.createElement("tr");
+    var numHours = parseInt(prompt("Enter the number of hours"));
+    var p = document.getElementById("test");
+    p.setAttribute("style","color:#32CD32");
+    p.innerHTML = "HOURS ADDED";
+    newCell.innerHTML = numHours;
+    newChild.appendChild(newCell);
+    user.appendChild(newChild);
+    
+}
+
+function showCodyHours() {
+    var table = document.getElementById("scores");
+    table.innerHTML = "";
+    var codyTr = document.createElement("tr");
+    var p = document.getElementById("test");
+    p.innerHTML = "Cody's Hours";
+    p.setAttribute("style","color:white");
+    for(var i = 0;i<codyHours.length;i++) {
+        var newTd = document.createElement("td");
+        newTd.innerHTML += codyHours[i];
+        codyTr.appendChild(newTd);
+    }
+    codyTr.setAttribute("id","curUser");
+    table.appendChild(codyTr);
+}
+
+function showMomHours() {
+    var table = document.getElementById("scores");
+    table.innerHTML = "";
+    var label = document.createElement("tr");
+    label.innerHTML = "Mom's Hours";
+    var momTr = document.createElement("tr");
+    var p = document.getElementById("test");
+    p.innerHTML = "Mom's Hours";
+    p.setAttribute("style","color:white");
+    for(var i = 0;i<momHours.length;i++) {
+        var newTd = document.createElement("td");
+        newTd.innerHTML += momHours[i];
+        momTr.appendChild(newTd);
+    }
+    momTr.setAttribute("id","curUser");
+    table.appendChild(momTr);
+}
+
+function showDadHours() {
+    var table = document.getElementById("scores");
+    table.innerHTML = "";
+    var label = document.createElement("tr");
+    label.innerHTML = "Dad's Hours";
+    var dadTr = document.createElement("tr");
+    var p = document.getElementById("test");
+    p.innerHTML = "Dad's Hours";
+    p.setAttribute("style","color:white");
+    for(var i = 0;i<dadHours.length;i++) {
+        var newTd = document.createElement("td");
+        newTd.innerHTML += dadHours[i];
+        dadTr.appendChild(newTd);
+    }
+    dadTr.setAttribute("id","curUser");
+    table.appendChild(dadTr);
+}
+
+function clearHours() {
+    var table = document.getElementById("scores");
+    table.innerHTML = "";
+    var label = document.createElement("tr");
+    label.innerHTML = "No hours";
+    table.appendChild(label);
+    var p = document.getElementById("test");
+    p.innerHTML = "Hours cleared";
+    p.setAttribute("style","color:white");
+}
